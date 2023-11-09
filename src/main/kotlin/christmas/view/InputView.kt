c0b3fbbe782bd7bb658f557fe2218ae9e912880b
hongjeZZ <ajyong316@gmail.com>
2023-11-10 02:04:50 +0900

    feat: 사용자로부터 주문 메뉴를 입력받는 기능 구현

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -8,7 +8,8 @@ class InputView {
         return Console.readLine()
     }
 
-    fun readMenu() {
+    fun readMenu(): String {
         println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+        return Console.readLine().trim()
     }
 }
