b1835bc2815062714969c9cc64b45fbf7cc09441
hongjeZZ <ajyong316@gmail.com>
2023-11-10 01:24:50 +0900

    feat: 프로그램 시작 문구를 출력 기능 구현

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,8 @@
 package christmas
 
+import christmas.domain.EventPlannerProgram
+
 fun main() {
-    TODO("프로그램 구현")
+    val eventPlannerProgram = EventPlannerProgram()
+    eventPlannerProgram.run()
 }
---
+++ src/main/kotlin/christmas/domain/EventPlannerProgram.kt
@@ -0,0 +1,13 @@
+package christmas.domain
+
+import christmas.view.InputView
+import christmas.view.OutputView
+
+class EventPlannerProgram {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    fun run() {
+        outputView.printProgramStartMessage()
+    }
+}
