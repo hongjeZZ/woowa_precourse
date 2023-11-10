6ddd4d92aae25da3b7ecd0a5acf3ba66a878f7c4
hongjeZZ <ajyong316@gmail.com>
2023-11-10 18:36:25 +0900

    feat: 메뉴를 검증하는 기능 구현

---
+++ src/main/kotlin/christmas/domain/Menu.kt
@@ -0,0 +1,31 @@
+package christmas.domain
+
+enum class Menu(
+    private val menuName: String,
+    private val price: Int,
+    private val type: String
+) {
+    MUSHROOM_SOUP("양송이수프", 6_000, "에피타이저"),
+    TAPAS("타파스", 5_500, "에피타이저"),
+    CAESAR_SALAD("시저샐러드", 8_000, "에피타이저"),
+    T_BONE_STEAK("티본스테이크", 55_000, "메인"),
+    BBQ_RIBS("바비큐립", 54_000, "메인"),
+    SEAFOOD_PASTA("해산물파스타", 35_000, "메인"),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, "메인"),
+    CHOCO_CAKE("초코케이크", 15_000, "디저트"),
+    ICE_CREAM("아이스크림", 5_000, "디저트"),
+    ZERO_COLA("제로콜라", 3_000, "음료"),
+    RED_WINE("레드와인", 60_000, "음료"),
+    CHAMPAGNE("샴페인", 25_000, "음료");
+
+    companion object {
+        fun hasMenu(name: String): Boolean {
+            return entries.any { it.menuName == name }
+        }
+
+        fun isBeverage(name: String): Boolean {
+            val menu = entries.find { it.menuName == name }
+            return menu?.type == "음료"
+        }
+    }
+}
---
+++ src/main/kotlin/christmas/validator/MenuValidator.kt
@@ -0,0 +1,23 @@
+package christmas.validator
+
+import christmas.domain.Menu
+
+class MenuValidator {
+    fun validate(menus: List<String>) {
+        requireValidMenu(menus)
+        requireNotDuplicatedMenu(menus)
+        requireNotOnlyBeverage(menus)
+    }
+
+    private fun requireValidMenu(menus: List<String>) {
+        require(menus.all { name -> Menu.hasMenu(name) }) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+    }
+
+    private fun requireNotDuplicatedMenu(menus: List<String>) {
+        require(menus.toSet().size == menus.size) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+    }
+
+    private fun requireNotOnlyBeverage(menus: List<String>) {
+        require(!menus.all { name -> Menu.isBeverage(name) }) { "[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요." }
+    }
+}
