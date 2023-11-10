01d7c4da5813ed41a001a7e287ae1b18864a8c29
hongjeZZ <ajyong316@gmail.com>
2023-11-10 18:36:57 +0900

    feat: 메뉴 개수를 검증하는 기능 구현

---
+++ src/main/kotlin/christmas/validator/MenuCountValidator.kt
@@ -0,0 +1,17 @@
+package christmas.validator
+
+class MenuCountValidator {
+    fun validate(menuCounts: List<Int>) {
+        requireValidCounts(menuCounts)
+        requireMenuCountLimit(menuCounts)
+    }
+
+    private fun requireValidCounts(menuCounts: List<Int>) {
+        require(menuCounts.all { count -> count >= 1 }) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+    }
+
+    private fun requireMenuCountLimit(menuCounts: List<Int>) {
+        require(menuCounts.sum() <= 20) { "[ERROR] 메뉴는 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요." }
+    }
+
+}
