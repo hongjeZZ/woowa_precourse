34c7dbd26b4a6cbd34cd1adca3f05f8b96686cf2
hongjeZZ <ajyong316@gmail.com>
2023-11-14 19:22:29 +0900

    test: Badge 클래스 테스트 코드 작성

---
+++ src/test/kotlin/christmas/domain/eventCalculator/BadgeTest.kt
@@ -0,0 +1,34 @@
+package christmas.domain.eventCalculator
+
+import christmas.domain.Badge
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class BadgeTest {
+    @ParameterizedTest
+    @CsvSource(
+        "0, NONE",
+        "4_999, NONE",
+        "5_000, STAR",
+        "14_999, STAR",
+        "15_000, TREE",
+        "19_999, TREE",
+        "20_000, SANTA",
+        "25_000, SANTA"
+    )
+    fun `뱃지를 반환하는 기능 테스트`(totalDiscount: Int, expectedBadge: Badge) {
+        assertThat(Badge.getBadge(totalDiscount)).isEqualTo(expectedBadge)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "NONE, 없음",
+        "STAR, 별",
+        "TREE, 트리",
+        "SANTA, 산타",
+    )
+    fun `뱃지 이름을 반환하는 기능 테스트`(badge: Badge, expectedName: String) {
+        assertThat(badge.getName(badge)).isEqualTo(expectedName)
+    }
+}
