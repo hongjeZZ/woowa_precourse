11a8e0a42946a64e24634efbfee9577f7358466e
hongjeZZ <ajyong316@gmail.com>
2023-11-15 23:27:00 +0900

    refactor: 코틀린 코딩 컨벤션을 적용

--- src/main/kotlin/christmas/util/Splitter.kt
+++ src/main/kotlin/christmas/util/Splitter.kt
@@ -1,11 +1,7 @@
 package christmas.util
 
 object Splitter {
-    fun splitByComma(string: String): List<String> {
-        return string.split(",")
-    }
+    fun splitByComma(string: String): List<String> = string.split(",")
 
-    fun splitByHyphen(string: String): List<String> {
-        return string.split("-")
-    }
+    fun splitByHyphen(string: String): List<String> = string.split("-")
 }
